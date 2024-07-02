1f55b8d36616fc46824322b723d2d6c8eaee9c7e
Gokul S <159005418+Gokuls2003@users.noreply.github.com>
2024-07-02 08:39:45 +0530

    Time: 4 ms (56.98%), Space: 5.6 MB (82.9%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.c
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -1,56 +1,20 @@
-char * intToRoman(int num){
-
-    char* s = (char*) malloc(sizeof(char)*20);
-    memset (s, 0, sizeof(char)*20);
-    int i = 0;
-    
-    while (num > 0) {
-        if (num >= 1000) {
-            s[i++] = 'M';
-            num -= 1000;
-        } else if (num >= 900) {
-            s[i++] = 'C';
-            s[i++] = 'M';
-            num -= 900;
-        } else if (num >= 500) {
-            s[i++] = 'D';
-            num -= 500;
-        } else if (num >= 400) {
-            s[i++] = 'C';
-            s[i++] = 'D';
-            num -= 400;
-        } else if (num >= 100) {
-            s[i++] = 'C';
-            num -= 100;
-        }else if (num >= 90) {
-            s[i++] = 'X';
-            s[i++] = 'C';
-            num -= 90;
-        } else if (num >= 50) {
-            s[i++] = 'L';
-            num -= 50;
-        } else if (num >= 40) {
-            s[i++] = 'X';
-            s[i++] = 'L';
-            num -= 40;
-        } else if (num >= 10) {
-            s[i++] = 'X';
-            num -= 10;
-        } else if (num >= 9) {
-            s[i++] = 'I';
-            s[i++] = 'X';
-            num -= 9;
-        } else if (num >= 5) {
-            s[i++] = 'V';
-            num -= 5;
-        } else if (num >= 4){
-            s[i++] = 'I';
-            s[i++] = 'V';
-            num -= 4;
-        } else if (num >= 1) {
-            s[i++] = 'I';
-            num -= 1;
-        }
+char* intToRoman(int num) {
+    char* ones[9] = {"I", "II", "III", "IV", "V", "VI", "VII","VIII", "IX"};
+    char* tens[9] = {"X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    char* hundreds[9] = {"C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    char* thousands[3]={"M","MM","MMM"};
+    char* ans = (char*)malloc(20 * sizeof(char));
+    if (ans == NULL) {
+        return NULL; // malloc failed
     }
-    return s;
+    ans[0]='\0';
+    if(num/1000!=0)
+    {strcat(ans,thousands[(num/1000-1)]);}
+    if((num%1000)/100!=0)
+    {strcat(ans,hundreds[((num%1000)/100)-1]);}
+    if((num%100)/10!=0)
+    {strcat(ans,tens[((num%100)/10)-1]);}
+    if(num%10!=0)
+    {strcat(ans,ones[(num%10)-1]);}
+    return ans;
 }
