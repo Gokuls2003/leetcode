ea7fd1631d183f0ed35855e389d3df6ffc59a925
Gokul S <159005418+Gokuls2003@users.noreply.github.com>
2024-07-19 13:38:46 +0530

    Time: 11 ms (59.95%), Space: 12.5 MB (10.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -5,55 +5,31 @@
  *     struct ListNode *next;
  * };
  */
-
-struct ListNode* makeNode(int num)
-{
-    struct ListNode* newNode = (struct ListNode*) malloc(sizeof(struct ListNode));
-
-    newNode->val = num;
-    newNode->next = NULL;
-    return newNode;
-}
-
-
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    int sum;
-    bool carry = false,dummy;
-    struct ListNode* newList = NULL;
-    struct ListNode* newNode = NULL;
+struct ListNode* addTwoNumbers(struct ListNode* a, struct ListNode* b) {
+    struct ListNode* result = NULL;
     struct ListNode* prev = NULL;
+    int carry = 0;
+    while (a || b) {
+        int sum = carry + (a ? a->val : 0) + (b ? b->val : 0);
+        carry = (sum >= 10) ? 1 : 0;
+        sum = sum % 10;
 
-    while(l1 != NULL || l2 != NULL )
-    {
-        if(l1 == NULL) sum = l2->val;
-        else if(l2 == NULL) sum = l1->val;
-        else sum = l1->val + l2->val;
+        struct ListNode* node = malloc(sizeof(struct ListNode));
+        node->val = sum;
+        node->next = NULL;
 
-        if(carry) 
-        {
-            sum++;
-            carry = false;
-        }
-            
-        if(sum >= 10)
-        {
-            int num = sum % 10;
-            carry = true;
-            newNode = makeNode(num);
-        }
-        if(!carry) newNode = makeNode(sum);
-        if(newList == NULL) newList = newNode;
-        else prev->next = newNode;
-        prev = newNode;
-        if(l1 != NULL) l1 = l1->next;
-        if(l2 != NULL) l2 = l2->next;
+        if (!result) result = node;
+        else prev->next = node;
+        prev = node;
+
+        if (a) a = a->next;
+        if (b) b = b->next;
     }
-    if(carry)
-    {
-        newNode = makeNode(1);
-        if(newList == NULL) newList = newNode;
-        else prev->next = newNode;
-        prev = newNode;
+    if (carry > 0) {
+        struct ListNode* node = malloc(sizeof(struct ListNode));
+        node->val = carry;
+        node->next = NULL;
+        prev->next = node;
     }
-    return newList;
-}   
+    return result;
+}
