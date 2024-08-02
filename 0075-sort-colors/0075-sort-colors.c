183dfaf53848f64d584b31fb609e42f3c665af7f
Gokul S <159005418+Gokuls2003@users.noreply.github.com>
2024-08-02 09:24:13 +0530

    Time: 3 ms (51.04%), Space: 9.6 MB (12.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.c
+++ 0075-sort-colors/0075-sort-colors.c
@@ -1,29 +1,15 @@
-void sortColors(int* nums, int numsSize){
-    int lo=0, hi=numsSize-1, i = 0, tmp;
-    while (i <= hi) {
-        if (nums[i] == 0) {
-            tmp = nums[lo];
-            nums[lo++] = nums[i];
-            nums[i] = tmp;
-        }
-        if (nums[i] == 2) {
-            tmp = nums[hi];
-            nums[hi--] = nums[i];
-            nums[i] = tmp;
-            continue;
-        }
-        ++i;
-    }
+void sortColors(int* nums, int numsSize)
+{
+    for(int i=0;i<numsSize;i++)
+    { 
+       int min=i;
+       for(int j=i+1;j<numsSize;j++)
+       {
+           if(nums[min]>nums[j])
+               min=j;
+       }
+        int temp=nums[i];
+        nums[i]=nums[min];
+        nums[min]=temp;
+    }   
 }
-
-/*
-void sortColors(int* nums, int numsSize){
-    int colours[3] = {0}, i, j = 0;
-    for (i = 0; i < numsSize; ++i) {
-        ++colours[nums[i]];
-    }
-    for (i = 0; i < 3; ++i) {
-        while(colours[i]-- > 0) nums[j++] = i;
-    }
-}
-*/
